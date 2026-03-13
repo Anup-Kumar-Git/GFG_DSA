c92f29a01f6704e29bea35d3f1156cff0e07ad5c
Anup Kumar <anupkumar2k22@gmail.com>
2026-03-13 17:20:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -1,14 +1,42 @@
+// class Solution {
+//   public:
+//     void pushZerosToEnd(vector<int>& arr) {
+//         // code here
+//         int j=0;
+//         for(int i=0;i<arr.size();i++){
+//             if(arr[i]!=0){
+//                 swap(arr[i],arr[j]);
+//                 j++;
+                
+//             }
+//         }
+//     }
+// };
+
 class Solution {
   public:
     void pushZerosToEnd(vector<int>& arr) {
-        // code here
-        int j=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]!=0){
-                swap(arr[i],arr[j]);
-                j++;
-                
+        vector<int> temp;
+        
+        // store positive elements
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] > 0){
+                temp.push_back(arr[i]);
             }
         }
+        
+        
+        // store zero
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] == 0){
+                temp.push_back(arr[i]);
+            }
+        }
+        
+        // // copy back to original array
+        // for(int i = 0; i < arr.size(); i++){
+        //     arr[i] = temp[i];
+        // }
+        arr=temp;
     }
 };
