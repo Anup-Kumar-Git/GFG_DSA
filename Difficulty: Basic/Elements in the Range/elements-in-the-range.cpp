8eed805104cfba66a52a6e2e7eb357864957396d
Anup Kumar <anupkumar2k22@gmail.com>
2026-03-15 17:57:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    bool check_elements(int arr[], int n, int A, int B) {
+        // Your code goes here
+        
+        int count = 0;
+        
+        for(int i = A; i <= B; i++){
+            bool found = false;
+            
+            for(int j = 0; j < n; j++){
+                if(arr[j] == i){
+                    found = true;
+                    break;
+                }
+            }
+            
+            if(!found)
+                return false;
+        }
+        
+        return true;
+        
+            
+        
+    }
+};
