20b7e41bdf6737877dd21134443c94c98f4fc307
Anup Kumar <anupkumar2k22@gmail.com>
2026-03-29 12:07:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Convert to Roman No/convert-to-roman-no.cpp
+++ Difficulty: Easy/Convert to Roman No/convert-to-roman-no.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string convertToRoman(int n) {
-        int value[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<int>value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string>symbol = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
         string result = "";
         
-        for(int i = 0; i < 13; i++) {
-            while(n >= value[i]) {
-                result += symbol[i];
-                n -= value[i];
+        for(int i=0;i<value.size();i++){
+            while(n>=value[i]){
+                result+=symbol[i];
+                n-=value[i];
+                
             }
         }
         
