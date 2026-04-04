3c34f5ff0fcaa9a6ddeaef9740166bfa57650316
Anup Kumar <anupkumar2k22@gmail.com>
2026-04-04 23:51:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<int> valueEqualToIndex(vector<int>& arr) {
+        // code here
+        vector<int>ans;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==i+1){
+                ans.push_back(i+1);
+            }
+        }return ans;
+    }
+};
